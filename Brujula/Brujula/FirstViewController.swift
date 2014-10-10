134689287ba4c85a47fbcc871e70d22c0811bd62
Rodolfo Castillo <rovidrio@comunidad.unam.mx>
2014-10-10 09:27:49 -0500

    Version Alpha
    Comienza el desarrollo de la app

---
+++ Brujula/Brujula/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Brujula
+//
+//  Created by Rodolfo Castillo on 07/10/14.
+//  Copyright (c) 2014 Rodolfo Castillo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
